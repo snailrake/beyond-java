7f230776bab6fdd7e9f47755ef2e8dcb88241898
kowalsky011 <kowalsky01@list.ru>
2025-08-18 16:24:10 +0400

    Работа с исключениями(1)

---
+++ src/main/java/org/hogwarts/hogwarts/school/Spell.java
@@ -0,0 +1,26 @@
+package org.hogwarts.hogwarts.school;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Spell {
+
+    private String title;
+    private int power;
+
+    public Spell(String title, int power) {
+        this.title = title;
+        setPower(power);
+    }
+
+    public void setPower(int power){
+        if (power >= 1 && power <= 7) {
+            this.power = power;
+        } else {
+            throw new IllegalArgumentException("The spell's power should be between 1 and 7");
+        }
+    }
+}
--- src/main/java/org/hogwarts/hogwarts/school/Student.java
+++ src/main/java/org/hogwarts/hogwarts/school/Student.java
@@ -8,13 +8,17 @@ public class Student {
     String[] specialSkills;
 
     public Student(String name, String faculty, int age, String[] specialSkills) {
-        if (specialSkills.length > 3) {
-            System.out.println("Student can have maximum 3 special skills");
+        if (age >= 11) {
+            if (specialSkills.length > 3) {
+                System.out.println("Student can have maximum 3 special skills");
+            } else {
+                this.name = name;
+                this.faculty = faculty;
+                this.age = age;
+                this.specialSkills = specialSkills;
+            }
         } else {
-            this.name = name;
-            this.faculty = faculty;
-            this.age = age;
-            this.specialSkills = specialSkills;
+            System.out.println("You can only enroll in Hogwarts from the age of 11 or more");
         }
     }
 
@@ -32,5 +36,13 @@ public class Student {
 
     public String[] getSpecialSkills() {
         return specialSkills;
+    }
+
+    public void castSpell (Spell spell){
+        int maxSpellPower = age - 10;
+        if (spell.getPower() > maxSpellPower) {
+            throw new SpellCastException(this.name + " still too young to use a spell " + spell.getTitle());
+        }
+        System.out.println(this.name + " use spell " + spell.getTitle());
     }
 }
