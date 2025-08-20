87e419e93bec603b4dc70b9e68af493f51eba3a3
kowalsky011 <kowalsky01@list.ru>
2025-08-20 13:59:23 +0400

    Работа с интерфейсами(1)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 import org.hogwarts.hogwarts.school.Student;
 import org.hogwarts.hogwarts.beast.Hippogriff;
+import org.hogwarts.hogwarts.Items.Car;
+import org.hogwarts.hogwarts.Items.Cloak;
 
 public class Main {
 
@@ -14,5 +16,29 @@ public class Main {
         Hippogriff twoParamsHippo = new Hippogriff("Duglas", "red");
         Hippogriff oneParamsHippo = new Hippogriff("Wylsacom");
         noParamsHippo.setName("Nameless");
+
+        Student student = new Student(
+                "Harry Potter",
+                "Gryffindor",
+                19,
+                new String[]{"forbidden spells"});
+
+        Car car = new Car(student, 45, true);
+
+        if (car.isFlying) {
+            car.becomeInvisible();
+        } else {
+            car.becomeVisible();
+        }
+
+        Cloak cloak = new Cloak(2, 1);
+
+        boolean hasPeopleUnderCloak = true;
+
+        if (hasPeopleUnderCloak) {
+            cloak.becomeInvisible();
+        } else {
+            cloak.becomeVisible();
+        }
     }
 }
